752d702c8de74c4ea0a29b1f77040f2d35b25135
lysu <sulifx@gmail.com>
2016-08-19 23:54:28 +0800

    add udp srv

---
+++ socket/i6d_ucase.h
@@ -0,0 +1,14 @@
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define BUF_SIZE 10
+
+#define PORT_NUM 50002
